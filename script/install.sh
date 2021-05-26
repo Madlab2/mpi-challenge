ef387c2731d2673eb6975cd39adb11e902aa779f
madlab2 <pnsm@arcor.de>
2021-05-26 16:36:11 +0200

    edited scripts

--- script/install.sh
+++ script/install.sh
@@ -3,8 +3,8 @@ sudo apt-get update
 sudo apt-get -y install build-essential
 sudo apt-get -y install cmake
 sudo apt-get -y install openmpi-bin openmpi-doc libopenmpi-dev
+rm -rf mpi-challenge
 git clone https://github.com/Madlab2/mpi-challenge.git
 cd mpi-challenge/build
 cmake .. && make 
 ctest -VV
-mpirun -np 1 ./main
