ed5af9e30b3fa3ba254516a3f4728387f65a8f1b
KarinaHeins <56360970+KarinaHeins@users.noreply.github.com>
2021-05-26 14:45:22 +0200

    Update ablaufNodes.sh

--- ablaufNodes.sh
+++ ablaufNodes.sh
@@ -5,5 +5,6 @@ sudo apt-get -y install cmake
 sudo apt-get -y install openmpi-bin openmpi-doc libopenmpi-dev
 git clone https://github.com/Madlab2/mpi-challenge.git
 cd mpi-challenge/build
-cmake .. && make (&& ctest -VV)
+cmake .. && make 
+&& ctest -VV
 mpirun -np 1 ./main
