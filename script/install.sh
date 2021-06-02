0593c6693ba48c9c33fc8d45fd88194f6c8726b7
KarinaHeins <56360970+KarinaHeins@users.noreply.github.com>
2021-06-02 08:33:00 +0200

    change the branch

--- script/install.sh
+++ script/install.sh
@@ -6,5 +6,6 @@ sudo apt-get -y install openmpi-bin openmpi-doc libopenmpi-dev
 rm -rf mpi-challenge
 git clone https://github.com/Madlab2/mpi-challenge.git
 cd mpi-challenge/build
+git checkout dev
 cmake .. && make 
 ctest -VV
