af0283c02cae01fbacdd1d6fdadf763415013b1a
KarinaHeins <56360970+KarinaHeins@users.noreply.github.com>
2021-06-02 09:47:26 +0200

    Update install.sh

--- script/install.sh
+++ script/install.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get -y install build-essential
-sudo apt-get -y install cmake
-sudo apt-get -y install openmpi-bin openmpi-doc libopenmpi-dev
+REQUIRED_PKG="build-essential"
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG|grep "install ok installed")
+echo Checking for $REQUIRED_PKG: $PKG_OK
+if [ "" = "$PKG_OK" ]; then
+  echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
+  sudo apt-get --yes install $REQUIRED_PKG 
+fi
+REQUIRED_PKG="cmake"
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG|grep "install ok installed")
+echo Checking for $REQUIRED_PKG: $PKG_OK
+if [ "" = "$PKG_OK" ]; then
+  echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
+  sudo apt-get --yes install $REQUIRED_PKG 
+fi
+REQUIRED_PKG="openmpi-bin"
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG|grep "install ok installed")
+echo Checking for $REQUIRED_PKG: $PKG_OK
+if [ "" = "$PKG_OK" ]; then
+  echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
+  sudo apt-get --yes install openmpi-bin openmpi-doc libopenmpi-dev 
+fi
 rm -rf mpi-challenge
 git clone https://github.com/Madlab2/mpi-challenge.git
 cd mpi-challenge/build
