3c29a8358474a9d650033ffb747c0e7cea8f25c5
KarinaHeins <56360970+KarinaHeins@users.noreply.github.com>
2021-06-02 09:30:50 +0200

    Update startFile.sh

--- script/startFile.sh
+++ script/startFile.sh
@@ -1,9 +1,8 @@
-
+#!/bin/bash
 n=$1
 # make sure command line arguments are passed to the script
 if [ $# -eq 0 ]
 then
-	echo "A shell script to print multiplication table."
 	echo "Usage : $0 number"
 	exit 1
 fi
