eee27ea847435abb1ae0b8c07f9150a4c9be1ee2
KarinaHeins <56360970+KarinaHeins@users.noreply.github.com>
2021-06-02 09:30:14 +0200

    Update startFile.sh

--- script/startFile.sh
+++ script/startFile.sh
@@ -1,6 +1,15 @@
+
+n=$1
+# make sure command line arguments are passed to the script
+if [ $# -eq 0 ]
+then
+	echo "A shell script to print multiplication table."
+	echo "Usage : $0 number"
+	exit 1
+fi
 git pull
 sudo chmod +x install.sh
-for h in {0..5}
+for h in {0..$n}
 do
 echo "ubuntu@$h"
 ssh ubuntu@worker$h 'bash -s' < install.sh
