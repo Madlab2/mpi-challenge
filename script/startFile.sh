124bc5102da0576675270326cbd059bec422882b
KarinaHeins <56360970+KarinaHeins@users.noreply.github.com>
2021-06-02 09:53:22 +0200

    Update startFile.sh

--- script/startFile.sh
+++ script/startFile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 git pull
 sudo chmod +x install.sh
-for h in {0..5}
+for h in {0..6}
 do
 echo "ubuntu@worker$h"
 ssh ubuntu@worker$h 'bash -s' < install.sh
@@ -9,4 +9,4 @@ done
 cd ../build
 cmake .. && make
 ctest -VV
-mpirun -n 7 -host master-node,worker0,worker1,worker2,worker3,worker4,worker5 ./main
+mpirun -n 8 -host master-node,worker0,worker1,worker2,worker3,worker4,worker5,worker6 ./main
