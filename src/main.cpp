94141b8de23b9b1ed11c8cca5b94f926f0245bbe
KarinaHeins <56360970+KarinaHeins@users.noreply.github.com>
2021-06-07 21:10:06 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -89,7 +89,7 @@ int main(int argc, char **argv) {
 						index_new_word++;
 					}
 					
-					if(new_word[size_new_word-1] != buf[end_word])
+					if(new_word[size_new_word] != buf[end_word])
 									      {
 									      	std::cout << "hier -> Worker" << rank << "  " << new_word[size_new_word] << " == " << buf[begin_word] << std::endl;
 									      }
