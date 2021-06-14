2d3b4cf4450e7b2819443fa83631b30c95240561
KarinaHeins <56360970+KarinaHeins@users.noreply.github.com>
2021-06-14 18:48:05 +0200

    Update startFile.sh

--- script/startFile.sh
+++ script/startFile.sh
@@ -11,4 +11,4 @@ cd ../build
 cmake .. && make
 ctest -VV
 echo "This script took $SECONDS seconds to execute"
-mpirun -n 8 -host master-node,worker0,worker1,worker2,worker3,worker4,worker5,worker6 ./main
+mpirun -n 16 -host master-node,worker0,worker1,worker2,worker3,worker4,worker5,worker6,worker7,worker8,worker9,worker10,worker11,worker12,worker13,worker14 ./main
